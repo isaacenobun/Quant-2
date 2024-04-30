23e8644d288c9ab876f9e0c1ba00ca2557e0bc76
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2024-04-30 01:55:46 +0100

    updated admin.py

--- Quant/bloq/admin.py
+++ Quant/bloq/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(block_model)
+admin.site.register(door_model)
+admin.site.register(window_model)
+admin.site.register(opening_model)
